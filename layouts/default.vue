5c86bda447fd28b77afa6b61dafed6a47bdfac7c
pgkuroneru <pgkuroneru@gmail.com>
2021-03-14 14:22:19 +0900

    トップページのレイアウト修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,6 @@
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
       <div class="container">
         <a class="navbar-brand" href="/">pgkuroneru blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-
-        </div>
       </div>
     </nav>
     <div class="container mt-5">
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,19 @@
   <main class="main">
     <h2 class="title">記事一覧</h2>
     <div class="articles row mt-3">
-      <div class="card mr-2" style="width: 30%;" v-for="content in contents" :key="content.id">
-        <nuxt-link :to="`/${content.id}`" class="card-link">
-          <img :src="`${content.ogpImage.url}`" class="card-img-top img-fluid" alt="...">
-          <div class="card-body">
-            <h3 class="card-title" style="font-size: 1.25em;">{{ content.title }}</h3>
-            <p class="card-subtitle mb-2 text-muted">
-              カテゴリ－：{{ content.category.name }}
-            </p>
-            <p class="card-subtitle mb-2 text-muted">投稿日：{{ new Date(content.publishedAt).toLocaleDateString('ja-JP', {timeZone: 'Asia/Tokyo'}) }}</p>
-          </div>
-        </nuxt-link>
+      <div class="col-md-4" v-for="content in contents" :key="content.id">
+        <div class="card mb-2">
+          <nuxt-link :to="`/${content.id}`" class="card-link">
+            <img :src="`${content.ogpImage.url}`" class="card-img-top img-fluid" alt="...">
+            <div class="card-body">
+              <h3 class="card-title" style="font-size: 1.25em;">{{ content.title }}</h3>
+              <p class="card-subtitle mb-2 text-muted">
+                カテゴリ－：{{ content.category.name }}
+              </p>
+              <p class="card-subtitle mb-2 text-muted">投稿日：{{ new Date(content.publishedAt).toLocaleDateString('ja-JP', {timeZone: 'Asia/Tokyo'}) }}</p>
+            </div>
+          </nuxt-link>
+        </div>
       </div>
     </div>
   </main>
